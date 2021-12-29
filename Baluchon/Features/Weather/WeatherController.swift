570d0d3a059bf2b265c881ac083cb131f76f8358
fred milloh <fredmilloh@icloud.com>
2021-12-29 10:49:47 +0100

    update weather UI

--- Baluchon/Features/Weather/WeatherController.swift
+++ Baluchon/Features/Weather/WeatherController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVKit
 
 class WeatherController: UIViewController {
     
@@ -13,10 +14,18 @@ class WeatherController: UIViewController {
     @IBOutlet weak var research: UITextField!
     @IBOutlet weak var destinationWeather: WeatherView!
     
+    var videoPlayer: AVPlayer?
+    var videoPlayerLayer: AVPlayerLayer?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        destinationWeather.isHidden = true
         getLocalWeather()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setupVideo()
     }
     
     private func getLocalWeather() {
@@ -48,16 +57,30 @@ class WeatherController: UIViewController {
                 
             }
         }
+        destinationWeather.isHidden = false
     }
+ 
+// MARK: - Player Video
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupVideo() {
+        
+        // obtenir le chemin d'accès à la resource vidéo
+        let bundlePath = Bundle.main.path(forResource: "SkyBackground", ofType: "mp4")
+        guard bundlePath != nil else { return }
+        
+        //créer une url à partir du chemin
+        let url = URL(fileURLWithPath: bundlePath!)
+        
+        
+        let item = AVPlayerItem(url: url)
+        videoPlayer = AVPlayer(playerItem: item)
+        videoPlayerLayer = AVPlayerLayer(player: videoPlayer)
+        
+        videoPlayerLayer?.frame = CGRect(x: -self.view.frame.size.width*1.5, y: 0, width: self.view.frame.size.width*4, height: self.view.frame.size.height)
+        
+        view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+        videoPlayer?.playImmediately(atRate: 0.3)
     }
-    */
+
 
 }
