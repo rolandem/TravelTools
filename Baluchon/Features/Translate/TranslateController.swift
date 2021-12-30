99a060bb92e7d2b27137ca65cf3f2b416b59fa51
fred milloh <fredmilloh@icloud.com>
2021-12-30 20:34:10 +0100

    implement TranslateController with minimal UI

--- Baluchon/Features/Translate/TranslateController.swift
+++ Baluchon/Features/Translate/TranslateController.swift
@@ -7,22 +7,37 @@
 
 import UIKit
 
-class TranslateController: UIViewController {
-
+class TranslateController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet weak var originalText: UITextView!
+    @IBOutlet weak var translatedText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func translateTexte(_ sender: UIButton) {
+        guard let inputText = originalText.text else { return }
+        
+        TranslateRequest.getLTranslation(inputText: inputText, sourceLang: "fr", targetLang: "en") { result in
+            switch result {
+            case .failure(let error) : print(error)
+            case .success(let translation) :
+                self.translatedText.text = translation.translationData.translations[0].translatedText
+            }
+        }
     }
-    */
+    
+    // MARK: - Keyboard
 
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        originalText.resignFirstResponder()
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        originalText.resignFirstResponder()
+        return true
+    }
+   
 }
